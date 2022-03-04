ebb08afecbc34a28e409d257511a95ecdc279a23
Rayan Edjekouane <rayan.edjekouane@efrei.net>
2022-03-04 21:14:00 +0100

    prom can't connect api-node

--- web/app.py
+++ web/app.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 from routes import configure_routes
 
+from flask import jsonify
+from prometheus_flask_exporter import PrometheusMetrics
+
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
 configure_routes(app)
+
+metrics.info("app_info", "App Info, this can be anything you want", version="1.0.0")
--- web/routes.py
+++ web/routes.py
@@ -1,6 +1,7 @@
 import requests
 from flask import request, render_template
 
+import time, random
 
 def configure_routes(app):
     @app.route('/')
@@ -10,8 +11,11 @@ def configure_routes(app):
     @app.route('/', methods=['POST'])
     def analyse():
         text_to_analyse = request.form['text']
-        api_url = "http://api-toxicity:5000/?data=" + text_to_analyse
+        api_url = "http://api-toxicity:5001/?data=" + text_to_analyse
         response = requests.get(api_url)
         json_response = response.json()
         return render_template('index.html', final=True, toxicity = json_response['toxicity'], severe_toxicity = json_response['severe_toxicity'],
          obscene = json_response['obscene'], threat = json_response['threat'], insult = json_response['insult'], identity = json_response['identity_attack'])
+
+
+
